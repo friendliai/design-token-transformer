6fa882b52f9cac534a32410d63d395a92091b8b1
siyoon <siyoon@friendli.ai>
2023-10-19 14:49:19 +0900

    cutoff unused

--- transformTokens.js
+++ transformTokens.js
@@ -1,167 +1,64 @@
-const StyleDictionary = require('style-dictionary')
-const deepMerge = require("deepmerge");
-const webConfig = require('./src/web/index.js')
-const androidConfig = require("./src/android/index.js");
-
-StyleDictionary.registerTransform({
-  name: 'size/px',
-  type: 'value',
-  matcher: token => {
-    return (token.unit === 'pixel' || token.type === 'dimension') && token.value !== 0
-  },
-  transformer: token => {
-    return `${token.value}px`
-  }
-})
-
-StyleDictionary.registerTransform({
-  name: 'size/percent',
-  type: 'value',
-  matcher: token => {
-    return token.unit === 'percent' && token.value !== 0
-  },
-  transformer: token => {
-    return `${token.value}%`
-  }
-})
+const StyleDictionary = require("style-dictionary");
 
 StyleDictionary.registerFilter({
-  name: 'validToken',
-  matcher: function(token) {
+  name: "validToken",
+  matcher(token) {
     return [
       "dimension",
       "string",
       "number",
-      "color",
       "custom-spacing",
-      "custom-gradient",
       "custom-fontStyle",
       "custom-radius",
-      "custom-shadow",
     ].includes(token.type);
-  }
-})
+  },
+});
 
+StyleDictionary.registerFilter({
+  name: "colorToken",
+  matcher(token) {
+    return ["color", "custom-gradient", "custom-shadow"].includes(token.type);
+  },
+});
+
+/**
+ * @type {import("style-dictionary").Core}
+ */
 const StyleDictionaryExtended = StyleDictionary.extend({
-  ...deepMerge.all([androidConfig, webConfig]),
   source: ["tokens/*.json"],
+  // https://github.com/lukasoppermann/design-token-transformer/blob/main/src/web/index.js
+  transform: {
+    "size/rem": {
+      type: "value",
+      matcher(token) {
+        return token.type === "dimension";
+      },
+      transformer(token) {
+        return `${Number(token.original.value) / 16}rem`;
+      },
+    },
+  },
+  transformGroup: {
+    "custom/json": StyleDictionary.transformGroup.css.concat(["size/rem"]),
+  },
   platforms: {
-    scss: {
-      transformGroup: "custom/css",
-      buildPath: "build/scss/",
+    json: {
+      buildPath: "build/",
+      transformGroup: "custom/json",
       files: [
         {
-          destination: "_variables.scss",
-          format: "scss/variables",
+          destination: "color.json",
+          format: "json/nested",
+          filter: "colorToken",
+        },
+        {
+          destination: "variables.json",
+          format: "json/nested",
           filter: "validToken",
-        },
-      ],
-    },
-    less: {
-      transformGroup: "custom/css",
-      buildPath: "build/less/",
-      files: [
-        {
-          destination: "_variables.less",
-          format: "less/variables",
-          filter: "validToken",
-        },
-      ],
-    },
-    css: {
-      transformGroup: "custom/css",
-      buildPath: "build/css/",
-      files: [
-        {
-          destination: "_variables.css",
-          format: "css/variables",
-          filter: "validToken",
-          options: {
-            showFileHeader: false,
-          },
-        },
-      ],
-    },
-    "json-flat": {
-      transformGroup: "js",
-      buildPath: "build/json/",
-      files: [
-        {
-          destination: "styles.json",
-          format: "json/flat",
-          filter: "validToken",
-        },
-      ],
-    },
-    ios: {
-      transformGroup: "ios",
-      buildPath: "build/ios/",
-      files: [
-        {
-          destination: "StyleDictionaryColor.h",
-          format: "ios/colors.h",
-          className: "StyleDictionaryColor",
-          type: "StyleDictionaryColorName",
-          filter: {
-            type: "color",
-          },
-        },
-        {
-          destination: "StyleDictionaryColor.m",
-          format: "ios/colors.m",
-          className: "StyleDictionaryColor",
-          type: "StyleDictionaryColorName",
-          filter: {
-            type: "color",
-          },
-        },
-        {
-          destination: "StyleDictionarySize.h",
-          format: "ios/static.h",
-          className: "StyleDictionarySize",
-          type: "float",
-          filter: {
-            type: "number",
-          },
-        },
-        {
-          destination: "StyleDictionarySize.m",
-          format: "ios/static.m",
-          className: "StyleDictionarySize",
-          type: "float",
-          filter: {
-            type: "number",
-          },
-        },
-      ],
-    },
-
-    "ios-swift-separate-enums": {
-      transformGroup: "ios-swift-separate",
-      buildPath: "build/ios-swift/",
-      files: [
-        {
-          destination: "StyleDictionaryColor.swift",
-          format: "ios-swift/enum.swift",
-          className: "StyleDictionaryColor",
-          filter: {
-            type: "color",
-          },
-        },
-        {
-          destination: "StyleDictionarySize.swift",
-          format: "ios-swift/enum.swift",
-          className: "StyleDictionarySize",
-          type: "float",
-          filter: {
-            type: "number",
-          },
         },
       ],
     },
   },
 });
-console.log('StyleDictionaryExtended', StyleDictionaryExtended)
 
-
-StyleDictionaryExtended.buildAllPlatforms()
+StyleDictionaryExtended.buildAllPlatforms();
