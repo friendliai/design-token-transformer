6ade9e6f0ff8f215108d77f631d97c713689b666
siyoon <siyoon@friendli.ai>
2023-11-07 11:13:37 +0900

    extract typography tokens

--- transformTokens.js
+++ transformTokens.js
@@ -1,23 +1,38 @@
 const StyleDictionary = require("style-dictionary");
 
+const validTypes = [
+  "dimension",
+  "string",
+  "number",
+  "custom-spacing",
+  "custom-fontStyle",
+  "custom-radius",
+];
+
+const fontPaths = ["articulat", "title", "mono", "body"];
+
+const colorTypes = ["color", "custom-gfontPathsradient", "custom-shadow"];
+
 StyleDictionary.registerFilter({
   name: "validToken",
   matcher(token) {
-    return [
-      "dimension",
-      "string",
-      "number",
-      "custom-spacing",
-      "custom-fontStyle",
-      "custom-radius",
-    ].includes(token.type);
+    return (
+      validTypes.includes(token.type) && !fontPaths.includes(token.path[0])
+    );
   },
 });
 
 StyleDictionary.registerFilter({
   name: "colorToken",
   matcher(token) {
-    return ["color", "custom-gradient", "custom-shadow"].includes(token.type);
+    return colorTypes.includes(token.type);
+  },
+});
+
+StyleDictionary.registerFilter({
+  name: "typoToken",
+  matcher(token) {
+    return fontPaths.includes(token.path[0]);
   },
 });
 
@@ -55,6 +70,11 @@ const StyleDictionaryExtended = StyleDictionary.extend({
           destination: "variables.json",
           format: "json/nested",
           filter: "validToken",
+        },
+        {
+          destination: "typography.json",
+          format: "json/nested",
+          filter: "typoToken",
         },
       ],
     },
